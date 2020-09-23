4a1dd297182ae83efa1458dd5442c933c11a305a
yuanqihang <qihang.yuan@thoughtworks.com>
2020-09-23 20:28:07 +0800

    complete ex3

--- src/exercise-1/components/App.js
+++ src/exercise-1/components/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import '../styles/App.css';
-import { BrowserRouter, Route, NavLink } from 'react-router-dom';
+import { BrowserRouter, Route, NavLink,Switch,Redirect } from 'react-router-dom';
 import Home from './Home.js';
 import My_profile from './My_profile.js';
 import About from './About_us.js';
@@ -25,11 +25,18 @@ class App extends Component {
             textDecoration: "underline"
           }} className="nav_link_body" to='/products' >Products</NavLink></div>
         </div>
+      <Switch>
         <Route exact path='/home' component={Home} />
         <Route exact path='/my_profile' component={My_profile} />
         <Route exact path='/about_us' component={About} />
         <Route exact path='/products' component={Products} />
-        <Route  path='/products/:id' component={Product} />
+        <Route path='/products/:id' component={Product} />
+        <Route path='/goods' render={
+            ()=>(<Redirect to="/products"/>)} />
+        <Route render={
+            ()=>(<Redirect to="/home"/>)}
+        />
+        </Switch>
       </BrowserRouter>
 
 
